c4c19bb2d0d6f6b9829f75f18f654e8441eb4899
Phung-B2005729 <phungb2005729@student.ctu.edu.vn>
2023-09-13 22:37:41 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+const ApiError = require("./app/api-error");
+// 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 app.get("/",(req, res)=>{
     res.json({
         message: "Welcom to contact book application."
